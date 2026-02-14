72dc05fcf4dd08baedd501ce9d6a528dacb32fde
kittiBank <kittitach.pro@gmail.com>
2026-02-14 16:33:00 +0700

    feat: integrate-auth-service-api

--- book-management-backend/index.js
+++ book-management-backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const bookRoutes = require("./routes/book.routes");
@@ -10,6 +11,7 @@ const swaggerJSDoc = require("swagger-jsdoc");
 
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(loggerMiddleware.logRequests);
 
